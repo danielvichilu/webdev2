bf99e23f16cb2f310251a2df95b37239b4587f50
danielvichilu <52408180+danielvichilu@users.noreply.github.com>
2019-11-06 16:33:28 +0200

    Update pc_config.js

--- public/js/pc_config.js
+++ public/js/pc_config.js
@@ -1,2 +1,8 @@
 // ICE Server Configgg
-let _PeerConnectionConfig ={'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'beaver','username': 'webrtc.websitebeaver@gmail.com'}]};
+// ICE Server Config
+
+let _PeerConnectionConfig = {
+  "iceServers":[
+    {"urls": "stun:stun.l.google.com:19302"}
+  ]
+};
