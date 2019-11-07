ad4272124b21fa1efe30e8a421ca7e91e1d5e916
danielvichilu <52408180+danielvichilu@users.noreply.github.com>
2019-11-07 20:07:50 +0200

    Update options.js

--- public/js/options.js
+++ public/js/options.js
@@ -7,7 +7,7 @@ module.exports =
     serverOptions: {
       // --- port and host name ---
       listenPort : process.env.PORT,
-      hostName: "ip.address()", // ex) server.domain.com
+      hostName: ip.address(), // ex) server.domain.com
       // --- for using HTTPS ---
       useHttps: false,
       httpsKeyFile: './key/server.key',
